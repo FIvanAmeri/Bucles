92a99f45c0693d6cc7594ca7db2655ca17bf7208
FIvanAmeri <dark.ranked@gmail.com>
2024-05-06 16:40:05 -0300

    Bucles

---
+++ 18.js
@@ -0,0 +1,23 @@
+function productoEntreNúmeros(a, b) {
+  // Dados dos argumentos "a" y "b", devuelve el producto de todos
+  // los números entre a y b (inclusive).
+  // Tu código:
+
+  
+  if (a === 0 || b === 0) {
+    return 0;
+  }
+
+  var producto = 1;
+  var inicio = Math.min(a, b);
+  var fin = Math.max(a, b);
+
+  for (var i = inicio; i <= fin; i++) {
+    producto *= i;
+  }
+  
+  return producto;
+}
+
+
+module.exports = productoEntreNúmeros;
